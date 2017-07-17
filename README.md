d7c3ea4966f12660f8241b2e14fa9bdcf63f6a61
h4ni7 <aizawahamd10@gmail.com>
2017-07-17 13:52:44 +0700

    resolve-conflict

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # resolve-conflict
+Latihan resolve conflict pada github dengan akun hanif10 sebagai upstream, yang lainnya sebagai kontributor :
+1. hanif10 (Hanif Isnain Musthafa)
---
+++ tugas.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <iostream.h>
+#include <conio.h>
+void baca_data(char A[],int n){
+	int i;
+	for (i=0;i<n;i++) {
+		cout<<"Data ke-"<<(i+1)<<" = ";
+		cin>>A[i]; }
+}
+void cetak_data(char A[],int n){
+	int i;
+	for (i=0;i<n;i++)
+	cout<<A[i]<<" "; cout<<endl;
+}
+void insertion_sort(char A[],int n){
+	int k,j,temp;
+	for (k=0;k<n;k++) {
+		temp=A[k];
+		j=k-1;
+		while ((temp<=A[j]) && (j>0)) {
+			A[j+1] = A[j];
+			j=j-1; }
+		if (temp>=A[j]) A[j+1]=temp;
+		else {
+			A[j+1] = A[j];
+			A[j] = temp; }
+	}
+}
+void main(){
+	char data[10];
+	int n;
+	cout<<"Banyak data : ";
+	cin>>n;
+	baca_data(data,n);
+	cetak_data(data,n);
+	insertion_sort(data,n);
+	cetak_data(data,n);
+	getch(); }
